980e0884302eb039af6239fae0cbd22064e758ed
Louay labidi <61356202+louayyy99@users.noreply.github.com>
2023-08-29 16:49:30 +0100

    Data dependencies ( Retrofit - Logging interceptor - Hilt - Timber )

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id ("com.google.dagger.hilt.android")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -16,11 +18,15 @@ android {
 
     buildTypes {
         release {
+            buildConfigField("String", "BASE_URL", "\"https://your_api_url_here\"")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            buildConfigField("String", "BASE_URL", "\"https://your_api_url_here\"")
         }
     }
     compileOptions {
@@ -41,4 +47,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // RETROFIT
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // LOGGING INTERCEPTOR
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    // HILT
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    // TIMBER
+    implementation("com.jakewharton.timber:timber:5.0.1")
 }
